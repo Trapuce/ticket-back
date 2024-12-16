15dd4044ae46861be121ee113f9053f8e71361eb
traore <dt2040760@gmail.com>
2024-12-16 18:05:14 +0100

    first commit

---
+++ src/main/java/com/trapuce/ticket_Back/TicketBackApplication.java
@@ -0,0 +1,13 @@
+package com.trapuce.ticket_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trapuce/ticket_Back/TicketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trapuce.ticket_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
